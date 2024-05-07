83573304687466df985a681284db1de6c50d0f1e
Paulo Santos <paulo.santos@compasso.com.br>
2024-05-07 14:24:32 -0300

    Add setUp and tearDown

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -3,14 +3,22 @@ import 'package:unit_test_flutter/controllers/list_controller.dart';
 import 'package:unit_test_flutter/models/todo.dart';
 
 void main() {
+  ListController listController = ListController();
+  setUp(() {
+    // This is run before each test
+  });
+
+  tearDown(() {
+    listController.clear();
+    // Clear the listController after each test
+  });
+
   group('TODO Controller', () {
     test('Initializes with empty', () {
-      ListController listController = ListController();
       expect(listController.todoList.length, 0);
     });
 
     test('TODO Added', () {
-      ListController listController = ListController();
       listController.addTodo(TodoModel("Unit Test", false));
       expect(listController.todoList.length, 1);
       expect(listController.todoList[0].content, "Unit Test");
